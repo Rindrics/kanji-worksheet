7352a9c6a4e956be4e0710bbd2eda748792d005e
Akira Hayashi <akira.hayashi@route06.co.jp>
2025-06-14 00:17:04 +0900

    Remove unnecessary kanji info

--- src/components/kanji/KanjiCard.tsx
+++ src/components/kanji/KanjiCard.tsx
@@ -9,8 +9,6 @@ export function KanjiCard({ kanji }: KanjiCardProps) {
   return (
     <Card className="text-center">
       <div className="text-4xl mb-2">{kanji.character}</div>
-      <div className="text-sm text-gray-600">{kanji.strokeCount}画</div>
-      <div className="text-xs text-gray-500">{kanji.radicals.join(', ')}</div>
     </Card>
   );
 }
