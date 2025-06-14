a79a4a9fc3213d2f2020e2d6a28a8a46d75a4a0a
Akira Hayashi <akira.hayashi@route06.co.jp>
2025-06-15 00:30:27 +0900

    Update app description

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
   SERVICE_NAME: 'なかま漢字ワークシート',
-  DESCRIPTION: '同じ構成要素を持つ漢字の練習プリントを作れます',
+  DESCRIPTION: 'テーマが共通する漢字を集めた練習プリントを作れます',
   SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || '',
   PDF_FILENAME_TEMPLATE: (groupName: string) => `${groupName} - ${APP_CONFIG.SERVICE_NAME}`,
   GITHUB: {
